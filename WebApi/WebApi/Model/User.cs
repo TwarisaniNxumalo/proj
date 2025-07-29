e5158b24be1051436235cdfa97097b335412503d
Twarisani <twarisani.nxumalo@lesakatech.com>
2025-07-29 20:18:53 +0200

    initial

---
+++ WebApi/WebApi/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace WebApi.Model
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public required string Name { get; set; }
+        public required  string Surname {  get; set; }
+        public required string Salt { get; set; }
+        public required string PasswordHash { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime UpdateAt { get; set; }
+    }
+}
---
+++ WebApi/WebApi/Model/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Model
+{
+    public class UserDto
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Salt { get; set; }
+        public required string PasswordHash { get; set; }
+        public required DateTime CreatedAt { get; set; }
+        public required DateTime UpdateAt { get; set; }
+    }
+}
